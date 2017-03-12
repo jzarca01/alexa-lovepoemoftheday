39df7f051f28b6ab0d0ae744efbb6548b7687481
Jérémie Zarca <jeremie@macbookair.home>
2017-03-13 00:10:58 +0100

    improved response

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const alexaSkillKit = require('alexa-skill-kit');
 const fetch = require('node-fetch');
 
-const apiUrl = "http://poetrydb.org/lines,linecount/love;2/lines.json";
+const apiUrl = "http://poetrydb.org/lines,linecount/love;2/author,title,lines.json";
 
 function getLovePoetryOfTheDay(url) {
     return fetch(url).then(res => {
         return res.json();
     }).then(response => {
+        return response.filter(poem => poem.lines.length < 30);
+    })
+    .then(response => {
         var numberOfPoems = response.length;
         var randomPoemId = (Math.random() * numberOfPoems - 1).toFixed();
         return response[randomPoemId];
-    })
-    .then(poem => {
-        return poem.lines.join(" ");
     });
 }
 
@@ -25,12 +25,12 @@ exports.handler = function(event, context) {
                     shouldEndSession: true,
                     outputSpeech: {
                     type: 'PlainText',
-                    text: poem
+                    text: poem.lines.join(" ")
                 },
                 card: {
                     type: 'Simple',
-                    title: 'Simple Card',
-                    content: 'An example for a simple card reply'
+                    title: `${poem.author} - ${poem.title}`,
+                    content: poem.lines.join(" ")
                 }
             }
         }
